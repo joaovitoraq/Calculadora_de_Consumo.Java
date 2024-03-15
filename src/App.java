5c9a1df1f3ecb45e28d99dcf35073e156f5e93eb
joaovitoraq <joaovitordeoliveiraaquino@gmail.com>
2024-03-15 08:20:12 -0300

    commit condicional nome dispositivo

--- src/App.java
+++ src/App.java
@@ -6,41 +6,53 @@ public class App{
         App app;
         app = new App ();
         
-        app.imprimeDados();
+        app.imprimeDados();  
+      }
+    
+    String aparelho = JOptionPane.showInputDialog("Nome do aparelho:");{
+
+        if (aparelho == null || aparelho.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nenhum nome de aparelho fornecido.");
+            System.exit(0);
+        } else if (!aparelho.matches("[a-zA-Z]+")) {
+          JOptionPane.showMessageDialog(null, "O nome do aparelho deve conter apenas letras.");
+          System.exit(0);
+        } else;
+      
       }
 
-    String aparelho = JOptionPane.showInputDialog("Nome do aparelho:"); // Nome do aparelho
+    String wattsInput = JOptionPane.showInputDialog("Digite os Watts do seu aparelho:");
+    double watts = Double.parseDouble(wattsInput); // potência em Watts do aparelho
 
     String tempInput = JOptionPane.showInputDialog("Digite o tempo de uso (Hora):");
     double temp_uso = Double.parseDouble(tempInput); // tempo em horas que esse aparelho é usado
 
-    String kwInput = JOptionPane.showInputDialog("Digite o consumo Kw/h do aparelho:");
-    double kwhora = Double.parseDouble(kwInput); // quanto esse apareho gasta por hora
+    int diasMes = Integer.parseInt(JOptionPane.showInputDialog("Dias do Mês (uso):"));
 
-    public double valor = ((kwhora * 0.71) * temp_uso) * 30;
+    public double valor = ((watts * temp_uso * diasMes / 1000) * 0.709);
 
     //int a = Integer.parseInt(JOptionPane.showInputDialog(""));
     DecimalFormat df = new DecimalFormat("#.##");
     String valorFormatado = df.format(valor);
 
     void imprimeDados(){
-        /*System.out.println("Aparelho: " +this.aparelho);
-        System.out.println("Tempo médio de uso: " +this.temp_uso);
-        //System.out.println("limite:" +this.limite);
-        System.out.println("Consome: " +this.kwhora);
-
-        System.out.println("Gasto mensal: " +this.valorFormatado);*/
-
-        //JOptionPane.showMessageDialog(null, "Gasto mensal: " + valorFormatado);
 
         String mensagem = "Nome do aparelho: " + aparelho + "\n" +
                           "Tempo médio de uso: " + temp_uso + " horas\n" +
-                          "Consome: " + kwhora + " Kw/h\n" +
+                          "Consome: " + watts + " Watts \n" +
                           "Gasto mensal: " + valorFormatado;
-        JOptionPane.showMessageDialog(null, mensagem);
+        JOptionPane.showMessageDialog(null, mensagem); 
+      }
     }
     
-}
+    
+
+  
+
+
+
+       
+
 
   
 
