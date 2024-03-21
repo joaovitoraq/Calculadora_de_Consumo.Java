004d7696c23afc860b5320d89b573b58e0afc6c9
joaovitoraq <joaovitordeoliveiraaquino@gmail.com>
2024-03-21 09:34:18 -0300

    commit loop aparelhos + soma final

--- src/App.java
+++ src/App.java
@@ -1,58 +1,72 @@
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-public class App{
+public class App {
     public static void main(String[] args) {
-        App app;
-        app = new App ();
-        
-        app.imprimeDados();  
-      }
-    
-    String aparelho = JOptionPane.showInputDialog("Nome do aparelho:");{
+        int numeroAparelhos = Integer.parseInt(JOptionPane.showInputDialog("Quantos aparelhos deseja calcular o consumo?"));
+
+        ArrayList<App> aparelhos = new ArrayList<>();
+
+        for (int i = 0; i < numeroAparelhos; i++) {
+            App app = new App();
+            app.perguntaDetalhes();
+            aparelhos.add(app);
+        }
+
+        // Calcula o gasto total do mês
+        double gastoTotal = 0;
+        for (App aparelho : aparelhos) {
+            gastoTotal += aparelho.valor;
+        }
+
+        // Imprime as informações de todos os aparelhos
+        StringBuilder mensagem = new StringBuilder();
+        for (App aparelho : aparelhos) {
+            mensagem.append(aparelho.getInfo()).append("\n\n");
+        }
+
+        mensagem.append("Gasto total do mês: ").append(new DecimalFormat("#.##").format(gastoTotal));
+
+        JOptionPane.showMessageDialog(null, mensagem.toString());
+    }
+
+    String aparelho;
+    double watts;
+    double temp_uso;
+    int diasMes;
+    double valor;
+    DecimalFormat df = new DecimalFormat("#.##");
+    String valorFormatado;
+
+    public void perguntaDetalhes() {
+        this.aparelho = JOptionPane.showInputDialog("Nome do aparelho:");
 
         if (aparelho == null || aparelho.isEmpty()) {
             JOptionPane.showMessageDialog(null, "Nenhum nome de aparelho fornecido.");
             System.exit(0);
-        } else if (!aparelho.matches("[a-zA-Z]+")) {
-          JOptionPane.showMessageDialog(null, "O nome do aparelho deve conter apenas letras.");
-          System.exit(0);
-        } else;
-      
-      }
+        } else if (!aparelho.matches("[a-zA-Z ]+")) {
+            JOptionPane.showMessageDialog(null, "O nome do aparelho deve conter apenas letras.");
+            System.exit(0);
+        }
 
-    String wattsInput = JOptionPane.showInputDialog("Digite os Watts do seu aparelho:");
-    double watts = Double.parseDouble(wattsInput); // potência em Watts do aparelho
+        String wattsInput = JOptionPane.showInputDialog("Digite os Watts do seu aparelho:");
+        this.watts = Double.parseDouble(wattsInput); // potência em Watts do aparelho
 
-    String tempInput = JOptionPane.showInputDialog("Digite o tempo de uso (Hora):");
-    double temp_uso = Double.parseDouble(tempInput); // tempo em horas que esse aparelho é usado
+        String tempInput = JOptionPane.showInputDialog("Digite o tempo de uso (Hora):");
+        this.temp_uso = Double.parseDouble(tempInput); // tempo em horas que esse aparelho é usado
 
-    int diasMes = Integer.parseInt(JOptionPane.showInputDialog("Dias do Mês (uso):"));
+        this.diasMes = Integer.parseInt(JOptionPane.showInputDialog("Dias do Mês (uso):"));
 
-    public double valor = ((watts * temp_uso * diasMes / 1000) * 0.709);
+        this.valor = ((watts * temp_uso * diasMes / 1000) * 0.709);
 
-    //int a = Integer.parseInt(JOptionPane.showInputDialog(""));
-    DecimalFormat df = new DecimalFormat("#.##");
-    String valorFormatado = df.format(valor);
-
-    void imprimeDados(){
-
-        String mensagem = "Nome do aparelho: " + aparelho + "\n" +
-                          "Tempo médio de uso: " + temp_uso + " horas\n" +
-                          "Consome: " + watts + " Watts \n" +
-                          "Gasto mensal: " + valorFormatado;
-        JOptionPane.showMessageDialog(null, mensagem); 
-      }
+        this.valorFormatado = df.format(valor);
     }
-    
-    
 
-  
-
-
-
-       
-
-
-  
-
+    public String getInfo() {
+        return "Nome do aparelho: " + aparelho + "\n" +
+                "Tempo médio de uso: " + temp_uso + " horas\n" +
+                "Consome: " + watts + " Watts \n" +
+                "Gasto mensal: " + valorFormatado;
+    }
+}
